61b2be6622cd76da6ce2ce04d8579513b9cf70f4
jason <jason@localhost.localdomain>
2023-10-01 23:03:46 +0530

    16_hisgrmG.c updtd

--- 16_hisgrmG.c
+++ 16_hisgrmG.c
@@ -1,56 +1,51 @@
 #include <stdio.h>
 #define IN 1
 #define OUT 0
-#define WORD_LIMIT 21
+#define WORD_SIZE 21
 
 /* print a histogram of the 
 lengths of words in its input.*/
 
 main(){
-    int c, i, j, state, nword, nchar, wc;
-    int ndata[WORD_LIMIT];
+    int c, i, j, nchar, nword, state;
+    int ndata[WORD_SIZE];
     state = OUT;
-    nword = nchar = wc = 0;
+    nchar = nword = 0;
 
-    for( i = 0; i < WORD_LIMIT-1; ++i){
+    for (i = 0; i < WORD_SIZE-1; ++i){
         ndata[i] = 0;
     }
 
-    while ((c = getchar()) != EOF){
-
-        if ((state == IN && (c != '\n'  && c != ' ')) || (state == OUT  && (c != '\n' && c != ' '))){
+    while((c=getchar()) != EOF){
+        if((state == IN && (c != ' ' && c != '\n')) || (state == OUT && (c != ' ' && c != '\n')) ){
             ++nchar;
-
         }
-        if ((state == IN && c == '\n') || (state == IN && c == ' ')){
+        if(state==IN && c == ' ' || c == '\n' ){
             ++ndata[nchar];
-            ++wc;
-            nchar =0;
+            nchar=0;
         }
-
-        if (c == ' ' || c == '\n' || c == '\t'){
+        if(c== ' ' || c == '\n' || c =='\t'){
             state = OUT;
         }
 
-        else if ( state == OUT){
+        else if(state == OUT){
             state = IN;
             ++nword;
         }
+        if(c == '\n'){
 
-        if (c == '\n'){
-            printf("\nWord length in Histogram\n");
-            printf("__________________________________________\n");
+            printf("\n World Lenght Historgram: \n");
+            printf("__________________________________\n");
 
-            for (i = 1; i < WORD_LIMIT-1 ; ++i){
+            for (i = 1; i < WORD_SIZE-1; ++i){
                 printf("%2d |", i);
                 for (j = 0; j < ndata[i]; ++j){
                     putchar('=');
                 }
                 putchar('\n');
             }
-            printf("__________________________________________\n");
+            printf("__________________________________\n");
+        
         }
-
     }
-
 }
