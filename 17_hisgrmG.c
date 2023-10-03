961a8890bca428944f1f7387d46db845d2da1d48
jason <jason@localhost.localdomain>
2023-10-03 16:22:21 +0530

    17-solved

--- 17_hisgrmG.c
+++ 17_hisgrmG.c
@@ -1,77 +1,76 @@
 #include <stdio.h>
-#define IN 1
-#define OUT 0
+#define IN 0
+#define OUT 1
 #define SIZE 21
-/* print a histogram of the 
-lengths of words in its input.
-011111011111110111110
-jason pandian jason
-12345'1234567'12345\
-a\n
-0 1 0
-b= i =2, 1
-j=1, 2
-2>=2 2>=1 
-0 ' 
-*/
+
+/*print word length as 
+histrogram(vertical)
+Go from all up then next ..*/
+
 main(){
-    int c, i, j, b, t, nchar, nword, state;
-    int wldata[SIZE];
+    int c, i, b, j, nword, nchar, state, wldata[SIZE];
     state = OUT;
-    nchar = nword = b = t = 0;
+    nword = nchar = b = 0;
 
-    for (i=1; i < SIZE; ++i){
+    for (i = 1; i < SIZE; ++i ){
         wldata[i] = 0;
-        printf("%d ", wldata[i]);
-
     }
-    printf("\n");
 
-    while ((c= getchar()) != EOF){
-        if (state == OUT && c != ' ' && c != '\n' || state == IN && c != ' ' && c != '\n'){
+    while (( c= getchar()) != EOF){
+        if(state == OUT &&  c != ' ' && c !='\n' || state == IN &&  c != ' ' && c !='\n'){
             ++nchar;
         }
-        if ( state == IN && c == ' ' || c == '\n'){
+        if(state == IN &&  c == ' ' || c =='\n'){
             ++wldata[nchar];
             nchar = 0;
         }
-        if (c==' ' || c=='\n' || c=='\t'){
+        if(c==' ' || c == '\n'){
             state = OUT;
         }
-        else if (state == OUT){
+
+        else if(state == OUT){
             state = IN;
             ++nword;
         }
 
-        if (c == '\n'){
-            /* printf("nword = %d \n", nword); */
-
-            printf("b = %d \n", b);
-
-            for (i=1; i < SIZE; ++i){
-                if (wldata[i]>=b){
-                    b = wldata[i];
+        if (c =='\n'){
+            printf("nword = %d \n", nword);
+        
+            for (i = 1; i < SIZE; ++i ){
+            printf(" %d ", wldata[i]);
+                if(wldata[i]>b){
+                    b=wldata[i];
                 }
-            }
-            printf("b = %d \n", b);
 
-            printf("WORD LENGTH HISTROGRAM \n");
-            for (i = b; i > 0; --i) {
-                for (j = 1; j < SIZE; ++j) {
-                    if (wldata[j] >= i) {
-                        printf("%3s","*");
-                    } else {
-                        printf("%3s"," ");
-                    }
-                }
-                printf("\n");
             }
-
-            for (i = 1; i < SIZE; ++i) {
-                printf("%3d", i);
-            }
-            b = 0;
             printf("\n");
+            printf("b = %d \n", b);
+
+            printf("WORD LENGTH HISTOGRAM VERTICAL \n");
+            
+            /*
+            * - - => top , we want to know the big - b and 1 2 3 4 ...  
+            * - *
+            1 2 4
+            wldata[j] >=i 5(wldata[i])>=5(j) 0>=5 .....
+                          5>=4 4>=4 0>=4 .....
+                          5>=3 4>=3 0>=3 .....
+             */
+            for ( i = b; i > 0; --i){
+                for (j = 1; j < SIZE; ++j){
+                    if (wldata[j]>=i){
+                        printf("%3s", "*");
+                    }
+                    else{
+                        printf("%3s", " ");
+                    }
+                }printf("\n");
+            }
+            for (i = 1; i < SIZE; ++i ){
+            printf("%3d", i);
+
+            }printf("\n");
+            b =0;
 
         }
     }
