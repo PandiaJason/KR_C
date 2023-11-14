69bf7f978abead3a9c08093f42885d651e8d7cc6
jason <jason@localhost.localdomain>
2023-11-15 00:30:08 +0530

    45,46,47

---
+++ 47_binsearch.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int binsearch(int x, int sa[], int n);
+void main()
+{
+    int x, n, i;
+    n = 10;
+    int sa[n];
+    for (i = 0; i < n; ++i) {
+        sa[i] = i+1;
+    }
+    binsearch(11, sa, n);
+}
+
+int binsearch(int x, int sa[], int n)
+{
+    int low, high, mid;
+
+    low = 0;
+    high = n - 1;
+
+    while (low <= high) {
+        mid = low + high / 2;
+        if (x < sa[mid]) {
+            high = mid + 1;
+        }
+
+        else if (x > sa[mid]) {
+            low = mid - 1;
+        }
+
+        else {
+            printf("The %d is found in sa[%d] location\n", x, mid);
+            return mid;
+        }
+
+    }
+    printf("The %d is not found in any sa location.\n", x);
+    return -1;
+}
