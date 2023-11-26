3a65f3e19a7c8283bd55036a50f8a414cd567c20
jason <jason@localhost.localdomain>
2023-11-26 17:58:42 +0530

    48,49

---
+++ 49_atoi.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <ctype.h>
+#define MAXSIZE 1000
+
+int atoi(char sArr[]);
+
+void main()
+{
+    char sArray[MAXSIZE];
+    int c, i;
+    for (i = 0; (c = getchar()) != EOF; ++i) {
+        sArray[i] = c;
+        if (c == '\n') {
+            break;
+        }
+    }
+    sArray[i] = '\0';
+
+    printf("atoi: %d \n", atoi(sArray));
+} 
+
+int atoi(char s[])
+{
+    int i, n, sign;
+
+    for (i = 0; isspace(s[i]); ++i) /* skip space */
+    ;
+    /* printf("%d\n", i); */
+    sign = (s[i] == '-') ? -1: 1; /* set -1 for -, else 1 for +. */
+    if (s[i] == '+' || s[i] == '-') { /* skip + or - */
+        ++i;
+    }
+
+    for (n = 0; isdigit(s[i]); ++i) {
+        n = 10 * n + (s[i] - '0');
+        /*printf("n: %d \n", n);*/
+    }
+
+    return sign * n;
+}
